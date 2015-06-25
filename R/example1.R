af245828bedd890deec3c3eae031e032b8fa6ed5
William Brannon <will.brannon@gmail.com>
2015-06-25 00:54:11 -0400

    doing example 1 in R

---
+++ R/example1.R
@@ -0,0 +1,11 @@
+## Example 1
+df <- read.csv("data/example1.csv")
+summary(df)
+dim(df)
+
+table(df$admit, useNA="ifany")
+table(df$rank, useNA="ifany")
+
+#so that the sql is simpler, we'll treat rank as a continuous variable
+summary(glm(admit ~ gre + gpa + rank, data=df, family=binomial("logit")))
+
